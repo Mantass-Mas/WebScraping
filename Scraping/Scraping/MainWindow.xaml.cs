25fe36fa7c0c08e50b34fce0b1d46178e231b286
Mantass-Mas <67854236+Mantass-Mas@users.noreply.github.com>
2020-09-13 01:07:05 +0900

    ・AngleSharpを追加 ・画面レイアウトのおおまかな決定 ・目標WebSiteのHTMLを文字列で取得する関数を作成(途中)

--- Scraping/Scraping/MainWindow.xaml.cs
+++ Scraping/Scraping/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
 
 namespace Scraping
 {
@@ -20,9 +23,29 @@ namespace Scraping
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            var month = DateTime.Today.Month;
+            monthText.Text = $"{month}月発売";
+        }
+    }
+
+    class WebData
+    {
+        static HttpClient client = new HttpClient();
+        public string[] StringData { get; set; }
+        public async Task GetString(int month)
+        {
+            var urlstring = @"http://yurinavi.com/yuri-calendar/";
+            var document = default(IHtmlDocument);
+            using(var stream = await client.GetStreamAsync(urlstring))
+            {
+                var parser = new HtmlParser();
+                document = await parser.ParseDocumentAsync(stream);
+            }
+            
         }
     }
 }
