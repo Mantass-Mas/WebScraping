0f17d8664b32f7f14e8d08d627a4cf9f14e53beb
万代 真通 <manDbcc794@gmail.com>
2020-09-21 16:59:34 +0900

    すっきり

--- Scraping/Scraping/MainWindow.xaml.cs
+++ Scraping/Scraping/MainWindow.xaml.cs
@@ -12,9 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Net.Http;
-using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
+
 
 namespace Scraping
 {
@@ -27,30 +25,7 @@ namespace Scraping
         public MainWindow()
         {
             InitializeComponent();
-            var month = DateTime.Today.Month;
-            monthText.Text = $"{month}月発売";
-        }
 
-        static void WindowCreate()
-        {
-
-        }
-    }
-
-    class WebData
-    {
-        static HttpClient client = new HttpClient();
-        public string[] StringData { get; set; }
-        public async Task GetString(int month)
-        {
-            var urlstring = @"http://yurinavi.com/yuri-calendar/";
-            var document = default(IHtmlDocument);
-            using(var stream = await client.GetStreamAsync(urlstring))
-            {
-                var parser = new HtmlParser();
-                document = await parser.ParseDocumentAsync(stream);
-            }
-            
         }
     }
 }
