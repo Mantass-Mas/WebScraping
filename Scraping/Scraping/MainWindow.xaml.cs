e010177dcbf65df95f4294c48bb7dea008216105
Mantass-Mas <67854236+Mantass-Mas@users.noreply.github.com>
2020-11-19 00:34:31 +0900

    注意）とりあえずの追加なのでエラーあり。 DataGridのTitleを選択した際の処理追加。 登録確認用のRegisterDialogを追加。

--- Scraping/Scraping/MainWindow.xaml.cs
+++ Scraping/Scraping/MainWindow.xaml.cs
@@ -44,5 +44,43 @@ namespace Scraping
                 }
             }
         }
+        private void dataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            var dataGrid = sender as DataGrid;
+
+            /// <summary>
+            /// DataGridのタイトルセルが選択されたとき、
+            /// MyBook登録確認ダイアログを表示
+            /// </summary>
+            if (dataGrid.CurrentColumn.Header.ToString() == "Title")
+            {
+                if (dataGrid.CurrentCell != null)
+                {
+                    //選択されたセルから純粋にタイトルのみを抽出する
+                    var cell = dataGrid.CurrentColumn.GetCellContent(dataGrid.CurrentItem) as TextBlock;
+                    var cell_text = cell.Text;
+                    cell_text = cell_text.Trim(' ', '(', ')');
+                    //var comic_num = 0;
+                    var end = cell_text[cell_text.Length - 1].ToString();
+                    var isNum = int.TryParse(end, out var num);
+                    while (isNum)
+                    {
+                        cell_text = cell_text.Remove(cell_text.Length - 1, 1);
+                        end = cell_text[cell_text.Length - 1].ToString();
+                        isNum = int.TryParse(end, out num);
+                    }
+                    var title = cell_text.Trim(' ', '(', ')');
+                    var window = new RegisterDialog();
+                    window.RegisterContent.Text = title;
+                    bool? res = window.ShowDialog();
+                    //MyBook登録確認ダイアログで登録が押された場合はtrueが返ってくる
+                    if (res == true)
+                    {
+                        //MyBooksにタイトルを追加
+                        Registered(title);
+                    }
+                }
+            }
+        }
     }
 }
