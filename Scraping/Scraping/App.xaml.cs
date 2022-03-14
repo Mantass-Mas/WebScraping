f2a9ca85f6f1bd53b073e9d7aed306ad425c654b
万代 真通 <manDbcc794@gmail.com>
2022-03-15 00:27:03 +0900

    Prism用ブランチ作成

---
+++ Scraping/Scraping/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Scraping.View;
+
+namespace Scraping
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        // 重複起動チェック用
+        private static Mutex _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // 重複起動チェック
+            Console.WriteLine("重複起動チェック開始");
+            _mutex = new Mutex(false, "{E35BC3FC-BE3F-4AD3-BCD4-8950ACC4A747}");
+            if(!_mutex.WaitOne(0, false))
+            {
+                _mutex.Close();
+                _mutex = null;
+                Console.WriteLine("重複起動してる");
+                Shutdown();
+                return;
+            }
+
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if(_mutex == null)
+            {
+                return;
+            }
+
+            // ミューテックスの解放
+            _mutex.ReleaseMutex();
+            _mutex.Close();
+            _mutex = null;
+        }
+    }
+}
---
+++ Scraping/Scraping/Model/DataBaseManager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using System.Data.SQLite.Linq;
+
+namespace Scraping.Model
+{
+    /// <summary>
+    /// データベース管理用クラス
+    /// </summary>
+    public class DataBaseManager
+    {
+        private static readonly string _fileName = @"favorite.sqlite3";
+        private readonly string _connectionString;
+
+        public DataBaseManager()
+        {
+            Console.WriteLine("コンストラクタ起動");
+            var builder = new SQLiteConnectionStringBuilder { DataSource =  _fileName};
+            _connectionString = builder.ToString();
+            using(var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                using(var cmd = conn.CreateCommand())
+                {
+                    // Tableがなければ作成
+                    Console.WriteLine("テーブル作成開始");
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS favorite (ID int PRIMARY KEY, Title string)";
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("テーブル作成終了");
+                }
+            }
+            Console.WriteLine("コンストラクタ終了");
+        }
+
+        /// <summary>
+        /// データベース書き込み用
+        /// </summary>
+        /// <param name="datas">登録するお気に入りリスト</param>
+        public void DataBaseWrite(IReadOnlyList<Data> datas)
+        {
+            Console.WriteLine("データベース書き込み");
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("データベース接続");
+                using (var cmd = conn.CreateCommand())
+                {
+                    // データの追加(書き込み)
+                    Console.WriteLine("データ書き込み開始");
+                    cmd.CommandText = @"SELECT * FROM favorite";
+                    var list = new List<int>();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read() == true)
+                        {
+                            int.TryParse(reader["ID"].ToString(), out int id);
+                            list.Add(id);
+                        }
+                    }
+                    foreach (var data in datas)
+                    {
+                        if (!(list.Contains(data.Id)))
+                        {
+                            cmd.CommandText = @"INSERT INTO favorite (ID, Title) VALUES (@ID, @Title)";
+                            cmd.Parameters.Add(new SQLiteParameter("@ID", data.Id));
+                            cmd.Parameters.Add(new SQLiteParameter("@Title", data.Title));
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    Console.WriteLine("データ書き込み終了");
+                }
+            }
+        }
+
+        /// <summary>
+        /// データベース読み込み用
+        /// </summary>
+        /// <returns>お気に入りデータ</returns>
+        public List<Data> DataBaseRead()
+        {
+            Console.WriteLine("データベース読み込み");
+            var list = new List<Data>();
+            using(var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("データベース接続");
+                using (var cmd = conn.CreateCommand())
+                {
+                    Console.WriteLine("データ読み込み開始");
+                    cmd.CommandText = @"SELECT * FROM favorite";
+                    using(var reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read() == true)
+                        {
+                            int.TryParse(reader["ID"].ToString(), out int id);
+                            var title = reader["Title"].ToString();
+                            var data = new Data()
+                            {
+                                Id = id,
+                                Title = title,
+                            };
+                            list.Add(data);
+                        }
+                    }
+                    Console.WriteLine("データ読み込み終了");
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 単体削除
+        /// </summary>
+        /// <param name="id">削除するID</param>
+        public void RemoveAt(int id)
+        {
+            Console.WriteLine("データ削除");
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("データベース接続");
+                using (var cmd = conn.CreateCommand())
+                {
+                    Console.WriteLine("削除処理開始");
+                    cmd.CommandText = @"DELETE FROM favorite WHERE ID = @ID";
+                    cmd.Parameters.Add(new SQLiteParameter("@ID", id));
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("削除処理終了");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 全削除
+        /// </summary>
+        public void RemoveAll()
+        {
+            Console.WriteLine("データ全削除");
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("データベース接続");
+                using (var cmd = conn.CreateCommand())
+                {
+                    Console.WriteLine("削除処理開始");
+                    cmd.CommandText = @"DELETE FROM favorite";
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("削除処理終了");
+                }
+            }
+        }
+    }
+}
